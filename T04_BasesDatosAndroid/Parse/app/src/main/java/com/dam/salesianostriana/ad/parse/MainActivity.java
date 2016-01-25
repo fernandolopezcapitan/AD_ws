7ee5d7b6461671c743828333cf2eacfe070e1a7f
fernandolopezcapitan <lopezcapitan.fernando@gmail.com>
2016-01-25 14:27:46 +0100

    Lunes

--- T04_BasesDatosAndroid/Parse/app/src/main/java/com/dam/salesianostriana/ad/parse/MainActivity.java
+++ T04_BasesDatosAndroid/Parse/app/src/main/java/com/dam/salesianostriana/ad/parse/MainActivity.java
@@ -3,18 +3,45 @@ package com.dam.salesianostriana.ad.parse;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import com.parse.GetCallback;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView lista;
+    List<Notas> notas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lista = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<Notas> adaptador = new ArrayAdapter<Notas>(this,
+                android.R.layout.simple_list_item_1,notas);
+
+        lista.setAdapter(adaptador);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Seleccionado: " + notas.get(position),
+                        Toast.LENGTH_SHORT).show();
+
+                //view.animate().setDuration(1000).rotationX(360);
+            }
+        });
 
 
 //        // Código de almacenamiento de un objeto
@@ -31,19 +58,43 @@ public class MainActivity extends AppCompatActivity {
         // Código de RECUPERACIÓN DE UN OBJETO
         // si es de un objeto, get(objectId)
         // si es de una lista de objeto, find()
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Todo");
+//        query.getInBackground("Dxq4MUzQ1b", new GetCallback<ParseObject>() {
+//            public void done(ParseObject object, ParseException e) {
+//                if (e == null) {
+//                    // object will be your game score
+//                    Log.d("FECHA", object.get("Fecha").toString());
+//                    Log.d("CONCEPTO", object.get("Concepto").toString());
+//                } else {
+//                    // something went wrong
+//                    Log.e("ERROR",Integer.toString(e.getCode()));
+//                }
+//            }
+//        });
+
+        // Codigo de recuperación de todos los objetos de una misma clase
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Todo");
-        query.getInBackground("Dxq4MUzQ1b", new GetCallback<ParseObject>() {
-            public void done(ParseObject object, ParseException e) {
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
                 if (e == null) {
-                    // object will be your game score
-                    Log.d("FECHA", object.get("Fecha").toString());
-                    Log.d("CONCEPTO", object.get("Concepto").toString());
+                    for (ParseObject object : objects) {
+                        Log.d("OBJECTID", object.getObjectId());
+                        Log.d("FECHA", object.get("Fecha").toString());
+                        Log.d("CONCEPTO", object.get("Concepto").toString());
+                    }
+
                 } else {
-                    // something went wrong
+
                     Log.e("ERROR",Integer.toString(e.getCode()));
                 }
             }
         });
+
+
+
+
+
 
 
     }
---
+++ T04_BasesDatosAndroid/Parse/app/src/main/java/com/dam/salesianostriana/ad/parse/Notas.java
@@ -0,0 +1,29 @@
+package com.dam.salesianostriana.ad.parse;
+
+/**
+ * Created by flopez on 25/01/2016.
+ */
+public class Notas {
+    private String fecha,concepto;
+
+    public Notas(String fecha, String concepto) {
+        this.fecha = fecha;
+        this.concepto = concepto;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+}
